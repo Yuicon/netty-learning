492b10d2684590c93fc1c528b7c66d98c6480239
Yuicon <910722178@qq.com>
2017-12-17 15:10:35 +0800

    feat:利用LineBasedFrameDecoder 解决TCP 粘包问题

--- src/netty/time/TimeServerHander.java
+++ src/netty/time/TimeServerHander.java
@@ -12,6 +12,8 @@ import java.util.Date;
  */
 public class TimeServerHander extends ChannelHandlerAdapter {
 
+    private int counter;
+
     @Override
     public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
         ctx.close();
@@ -19,12 +21,11 @@ public class TimeServerHander extends ChannelHandlerAdapter {
 
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-        ByteBuf buf = (ByteBuf) msg;
-        byte[] req = new byte[buf.readableBytes()];
-        buf.readBytes(req);
-        String body = new String(req, "UTF-8");
+        String body = (String) msg;
+        System.out.println("The time server receive order : " + body + "; the counter is : " + ++counter);
         String currentTime = "QUERY TIME ORDER".equalsIgnoreCase(body) ?
                 new Date(System.currentTimeMillis()).toString() : "BAD ORDER";
+        currentTime = currentTime + System.getProperty("line.separator");
         ByteBuf resp = Unpooled.copiedBuffer(currentTime.getBytes());
         ctx.write(resp);
     }
