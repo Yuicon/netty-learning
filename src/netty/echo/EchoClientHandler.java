7993ae33714a8c16e31b8780b1a74e6bdad48f2c
Yuicon <910722178@qq.com>
2017-12-17 16:09:14 +0800

    分隔符解码器的应用

---
+++ src/netty/echo/EchoClientHandler.java
@@ -0,0 +1,44 @@
+package netty.echo;
+
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerAdapter;
+import io.netty.channel.ChannelHandlerContext;
+
+/**
+ * @author Yuicon
+ */
+public class EchoClientHandler extends ChannelHandlerAdapter {
+
+    private int counter;
+
+    static final String ECHO_REQ = "Hi, Yuicon . Welcome to Netty.$_";
+
+    /**
+     * Creates a client-side handler
+     */
+    public EchoClientHandler() {
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        for (int i = 0; i < 10; i++) {
+            ctx.writeAndFlush(Unpooled.copiedBuffer(ECHO_REQ.getBytes()));
+        }
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        System.out.println("This is " + ++counter + "times receive server : [" + msg +  "]");
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        ctx.flush();
+    }
+}
