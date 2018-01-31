54fc8b8ac7fc5c93e3f7ad41f69233a75d58961f
Yuicon <910722178@qq.com>
2018-01-31 21:56:47 +0800

    消息编码

---
+++ src/io/netty/agreement/MarshallingCodecFactory.java
@@ -0,0 +1,42 @@
+package io.netty.agreement;
+
+import org.jboss.marshalling.*;
+
+import java.io.IOException;
+
+/**
+ * @author Yuicon
+ */
+public class MarshallingCodecFactory {
+
+    /**
+     * 创建Jboss Marshaller
+     *
+     * @return
+     * @throws IOException
+     */
+    protected static Marshaller buildMarshalling() throws IOException {
+        final MarshallerFactory marshallerFactory = Marshalling
+                .getProvidedMarshallerFactory("serial");
+        final MarshallingConfiguration configuration = new MarshallingConfiguration();
+        configuration.setVersion(5);
+        return marshallerFactory
+                .createMarshaller(configuration);
+    }
+
+    /**
+     * 创建Jboss Unmarshaller
+     *
+     * @return
+     * @throws IOException
+     */
+    protected static Unmarshaller buildUnMarshalling() throws IOException {
+        final MarshallerFactory marshallerFactory = Marshalling
+                .getProvidedMarshallerFactory("serial");
+        final MarshallingConfiguration configuration = new MarshallingConfiguration();
+        configuration.setVersion(5);
+        return marshallerFactory
+                .createUnmarshaller(configuration);
+    }
+
+}
